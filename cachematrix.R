bdc9dad9eff6d06ec5e4599f60c2f4777e620ffd
wallykmax <wallykmax@gmail.com>
2015-03-21 16:02:40 -0600

    Working version of the two functions with complete comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Two functions are defined here to compute the inverse of a matrix in a way 
+# to save computations if repeated evaluations are expected.
+#
+# makeCacheMatrix(), creates a special list of functions to set the value of a 
+# matrix, get the value of the matrix, set the value of the inverse and get 
+# the value of the inverse.
+#
+# cacheSolve(x), takes the list defined with makeCacheMatrix() with a value of
+# the matrix assigned with the set function, and returns the inverse. The 
+# inverse is only computed the first time the function is called. Sucessive 
+# calls use a cached value of the inverse, saving computations.
+#
+# Usage example:
+#
+# > x <- makeCacheMatrix(x=matrix(c(1/2,0,0,1/2), nrow=2, ncol=2))
+# > cacheSolve(x)
+#
+# The solution can be verified with
+#
+# > x$get() %*% x$getinv()
+#
+# that should return the identity matrix of appropriate dimensions 
+#
 
-## Write a short comment describing this function
-
+# makeCacheMatrix returns a list of functions to
+# 1.- set the value of a matrix (list$set)
+# 2.- get the value of a matrix (list$get)
+# 3.- set the inverse of a matrix (list$setinv)
+# 4.- get the inverse of a matrix (list$getinv)
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() inv
+    list(set = set, 
+         get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
+# cacheSolve returns the inverse of the matrix stored in the list x, which is 
+# defined with makeCacheMatrix.
+# The value of the inverse is only computed the first time the function is 
+# called. Succesive calls use a cached value saved during the first call.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    inv
 }
